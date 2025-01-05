330f01b79a63bf2a9d74ce0d808345edcab390a5
Michael Makukha <m.makukha@gmail.com>
2025-01-05 12:56:19 +0200

    Update package versions table generator

--- .dev/gen-package-versions.sh
+++ .dev/gen-package-versions.sh
@@ -2,6 +2,7 @@
 
 IMG=makukha/multipython
 
+RELEASE="$1"
 SUBSET="latest stable supported"
 PARTIAL="py314t py313t py314 py313 py312 py311 py310 py39 py38 py37 py36 py35 py27"
 PKG=(pip setuptools tox virtualenv)
@@ -11,32 +12,41 @@ mkdir -p tmp/info
 
 # get latest versions
 LATEST=()
-docker run --rm $IMG:stable py info -c > tmp/info/stable.json
+docker run --rm "$IMG:stable-$RELEASE" py info -c > tmp/info/stable.json
 for (( i = 0; i < ${#PKG[@]}; i++ ))
 do
   LATEST+=("$(jq -r '.python[] | select(.is_system==true) | .packages.'"${PKG[$i]}" "tmp/info/stable.json")")
 done
 
 # header
-for col in Tag ${PKG[@]}; do echo -n "| $col "; done; echo "|"
-for col in Tag ${PKG[@]}; do echo -n "|---"; done; echo "|"
+echo -n "| Tag "
+for col in "${PKG[@]}"; do echo -n "| $col "; done; echo "|"
+echo -n "|---"
+for col in "${PKG[@]}"; do echo -n "|---"; done; echo "|"
 
 print_rows () {
-  read -ar TAGS <<<"$1"
-  for tag in "${TAGS[@]}"
+  for tag in $1
   do
-    if [ "$tag" != "stable" ]; then
-      docker run --rm "$IMG:$tag" py info -c > "tmp/info/$tag.json"
+    # copy json metadata
+    if [ "$tag" = "latest" ]; then
+      docker run --rm "$IMG:$RELEASE" py info -c > "tmp/info/$tag.json"
+    elif [ "$tag" = "stable" ]; then
+      true  # already copied
+    else
+      docker run --rm "$IMG:$tag-$RELEASE" py info -c > "tmp/info/$tag.json"
     fi
+    # output rows
     echo -n "| "'`'"$tag"'`'" "
     for (( i = 0; i < ${#PKG[@]}; i++ ))
     do
       V="$(jq -r ".python[] | select(.is_system==true) | .packages.${PKG[$i]}" "tmp/info/$tag.json")"
-      echo "| $V"
+      echo -n "| $V"
       if [ "$V" = "${LATEST[$i]}" ]; then printf ' \xE2\x9C\xA8 '; else printf ' '; fi
     done
     echo "|"
+    echo -n '.' 1>&2  # progress
   done
+  echo 1>&2  # progress
 }
 
 # body: subsets
