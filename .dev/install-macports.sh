6ed4aed44f990e8610277c473fbb5966bc440cd7
Michael Makukha <m.makukha@gmail.com>
2025-01-14 12:35:25 +0200

    Add mypy and ruff

--- .dev/install-macports.sh
+++ .dev/install-macports.sh
@@ -5,4 +5,6 @@ sudo port install go-task hadolint jq shellcheck uv yq
 uv tool install -U git+https://github.com/makukha/bump-my-version@date62
 uv tool install -U caseutil
 uv tool install -U docsub
+uv tool install -U mypy
+uv tool install -U ruff
 uv tool install -U towncrier
